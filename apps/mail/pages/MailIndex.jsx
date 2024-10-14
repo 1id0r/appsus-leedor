bdfe9c0bed043968c5d12d68bda8717680d59c24
1id0r <lidornissim44@gmail.com>
2024-10-14 11:38:13 +0300

    text 2

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,18 +1,19 @@
-const { Link, Outlet }= ReactRouterDOM
+const { Link, Outlet } = ReactRouterDOM
 
-import { MailFilter } from "../cmps/MailFilter.jsx";
-import { MailFolderList } from "../cmps/MailFolderList.jsx";
-import { MailList } from "../cmps/MailList.jsx";
-
+import { MailFilter } from '../cmps/MailFilter.jsx'
+import { MailFolderList } from '../cmps/MailFolderList.jsx'
+import { MailList } from '../cmps/MailList.jsx'
 
 export function MailIndex() {
-    return <div>mail app
-        <h4>Hey</h4>
-        <MailFilter/>
-        <MailFolderList/>
-        <MailList/>
-        <Link to="/mail/compose" >New Mail</Link>
-        <Outlet/>
+  return (
+    <div>
+      mail app
+      <h4>Hey lee</h4>
+      <MailFilter />
+      <MailFolderList />
+      <MailList />
+      <Link to='/mail/compose'>New Mail</Link>
+      <Outlet />
     </div>
+  )
 }
-
